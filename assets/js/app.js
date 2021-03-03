4ccd9079b8dc72cd19b4d660a7ffefbd0be13101
Sumit Patel <sumitz1207@gmail.com>
2021-03-03 18:47:11 -0500

    added svg wrapper and height and width attributes

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,4 +6,17 @@ function updateChart() {
   right: 40,
   left:100
   };
+
+  let w = 960 - margin.right - margin.left;
+  let h = 500 - margin.bottom - margin.top;
+  
+  //make wrapper and append the svg group using the calculated height and width
+  let wrapper = d3.select("#scatter")
+      .append("svg")
+      .attr("width", 960)
+      .attr("height", 500);
+  //set graphData to wrapper 
+  let graphData = wrapper.append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
 }
